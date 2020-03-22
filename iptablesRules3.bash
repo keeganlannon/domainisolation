bdba878413511b030d0ae0024d516bfbcc2c0674
keeganlannon <62343261+keeganlannon@users.noreply.github.com>
2020-03-22 18:31:46 -0400

    adding 2nd version of iptables rues script
    This is a script that sets iptables rules that allows 3 hosts to communicate via ipsec and icmp. This script is running on centos03 in our environment.

--- iptablesRules3.bash
+++ iptablesRules3.bash
@@ -24,8 +24,27 @@ iptables -A OUTPUT -s $centos03 -d $centos01 -p udp --sport 1701 --dport 1701 -j
 iptables -A OUTPUT -s $centos03 -d $centos01 -p esp -j ACCEPT 
 iptables -A OUTPUT -s $centos03 -d $centos01 -p ah -j ACCEPT
 
-# Allow centos01 and centos02 to ping each other 
+# Allow IPsec communication between centos02 and centos03 
+iptables -A INPUT -s $centos02 -d $centos03 -p udp --sport 4500 --dport 4500 -j ACCEPT
+iptables -A INPUT -s $centos02 -d $centos03 -p udp --sport 500 --dport 500 -j ACCEPT
+iptables -A INPUT -s $centos02 -d $centos03 -p udp --sport 1701 --dport 1701 -j ACCEPT
+iptables -A INPUT -s $centos02 -d $centos03 -p esp -j ACCEPT
+iptables -A INPUT -s $centos02 -d $centos03 -p ah -j ACCEPT
+iptables -A OUTPUT -s $centos03 -d $centos02 -p udp --sport 4500 --dport 4500 -j ACCEPT
+iptables -A OUTPUT -s $centos03 -d $centos02 -p udp --sport 500 --dport 500 -j ACCEPT
+iptables -A OUTPUT -s $centos03 -d $centos02 -p udp --sport 1701 --dport 1701 -j ACCEPT
+iptables -A OUTPUT -s $centos03 -d $centos02 -p esp -j ACCEPT
+iptables -A OUTPUT -s $centos03 -d $centos02 -p ah -j ACCEPT
+
+# Allow centos01 and centos03 to ping each other 
 iptables -A INPUT -s $centos01 -d $centos03 -p icmp --icmp-type echo-reply -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -s $centos01 -d $centos03 -p icmp --icmp-type echo-request -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -s $centos03 -d $centos01 -p icmp --icmp-type echo-request -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -s $centos03 -d $centos01 -p icmp --icmp-type echo-reply -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
+# Allow centos02 and centos03 to ping each other 
+iptables -A INPUT -s $centos02 -d $centos03 -p icmp --icmp-type echo-reply -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -s $centos02 -d $centos03 -p icmp --icmp-type echo-request -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -s $centos03 -d $centos02 -p icmp --icmp-type echo-request -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -s $centos03 -d $centos02 -p icmp --icmp-type echo-reply -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
